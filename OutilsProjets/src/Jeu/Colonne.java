a920bf33970cb2e330c1d72f0589374cac275eab
ElouanGui <elouan.guillerm1@gmail.com>
2026-04-29 11:35:45 +0200

    Ajouts class

---
+++ OutilsProjets/src/Jeu/Colonne.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jeu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author eguiller
+ */
+public class Colonne {
+    
+    private ArrayList<Tuile> lesTuiles;
+    
+}
