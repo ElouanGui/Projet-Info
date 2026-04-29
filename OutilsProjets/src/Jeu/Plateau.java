701d7e62a8d46d31f5e43c573832037b2c685d93
aylalgaultier <aylalgaultier06@gmail.com>
2026-04-29 11:39:44 +0200

    Update Plateau.java

--- OutilsProjets/src/Jeu/Plateau.java
+++ OutilsProjets/src/Jeu/Plateau.java
@@ -9,5 +9,59 @@ package Jeu;
  * @author eguiller
  */
 public class Plateau {
+    private Colonne[] lesColonnes;
+    private int nbCol;
+    private int nbLig;
+    private int nbTypesTuiles;
     
+    
+    public Plateau(int nbCol, int nbLig, int nbTypesTuiles){
+        this.nbCol=nbCol;
+        this.nbLig=nbLig;
+        this.nbTypesTuiles=nbTypesTuiles;
+        this.lesColonnes=new Colonne [nbCol];
+        for (int i =0; i<this.nbCol;i++){
+            this.lesColonnes[i]=new Colonne(nbLig,nbTypes);
+        }
+    }
+    
+    public String toString(){
+        String str="";
+        for(int col=0; col<this.nbCol;col++){
+            for(int col=0; col<this.nbCol;col++){
+                str+=this.getTuiles(col, lig);
+            }
+        str+="\n";
+        }
+        return str;
+    }
+    
+    public Coord posMatchVertical(){
+        Coord pos = new Coord(-1,-1);
+        int col=0;
+        boolean trouve = false;
+        while (col<this.nbCol && !trouve){
+            int lig=0;
+            while (lig<this.nbLig-2 && !trouve){
+                if (this.existeMatch(new Coord(col,lig))){
+                    trouve=true;
+                    pos=new Coord(col,lig);
+                }else{
+                    lig++;
+                }
+            }
+            col++;
+        }
+        return pos;
+    }
+    
+    
+
+
+
+
+
+
+
 }
+
